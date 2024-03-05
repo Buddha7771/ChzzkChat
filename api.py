99734b991b9aa593d82f106e788e88e5ce34068d
Lee2532 <seungoh0423@gmail.com>
2024-03-06 00:46:07 +0900

    ADD : requests headers

--- api.py
+++ api.py
@@ -1,41 +1,45 @@
 import requests
 
 
-def fetch_chatChannelId(streamer:str) -> str :
-
+def fetch_chatChannelId(streamer: str) -> str:
     url = f'https://api.chzzk.naver.com/polling/v2/channels/{streamer}/live-status'
     try:
-        response = requests.get(url).json()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+        response = requests.get(url, headers=headers).json()
         return response['content']['chatChannelId']
     except:
         raise ValueError(f'잘못된 입력값 : {streamer}')
 
 
-def fetch_channelName(streamer:str) -> str :
-
+def fetch_channelName(streamer: str) -> str:
     url = f'https://api.chzzk.naver.com/service/v1/channels/{streamer}'
     try:
-        response = requests.get(url).json()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+        response = requests.get(url, headers=headers).json()
         return response['content']['channelName']
     except:
         raise ValueError(f'잘못된 입력값 : {streamer}')
 
 
-def fetch_accessToken(chatChannelId, cookies:dict) -> str :
-
-    url = f'https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId={chatChannelId}&chatType=STREAMING'    
+def fetch_accessToken(chatChannelId, cookies: dict) -> str:
+    url = f'https://comm-api.game.naver.com/nng_main/v1/chats/access-token?channelId={chatChannelId}&chatType=STREAMING'
     try:
-        response = requests.get(url, cookies=cookies).json()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+        response = requests.get(url, cookies=cookies, headers=headers).json()
         return response['content']['accessToken'], response['content']['extraToken']
     except:
         raise ValueError(f'잘못된 입력값 : {chatChannelId}, {cookies}')
 
 
-def fetch_userIdHash(cookies:dict) -> str :
-
+def fetch_userIdHash(cookies: dict) -> str:
     try:
-        response = requests.get('https://comm-api.game.naver.com/nng_main/v1/user/getUserStatus', cookies=cookies).json()
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'}
+        response = requests.get('https://comm-api.game.naver.com/nng_main/v1/user/getUserStatus', cookies=cookies,
+                                headers=headers).json()
         return response['content']['userIdHash']
     except:
         raise ValueError(f'잘못된 입력값 : {cookies}')
-    
